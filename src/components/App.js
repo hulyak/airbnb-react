48137037df454a8741be8e1b80103ee66e272337
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-18 11:05:36 -0700

    set up the project, import material ui

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header/Header';
+import Home from './Home/Home';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
