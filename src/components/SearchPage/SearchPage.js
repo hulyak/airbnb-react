7aaf3ab01f31489d4b0f43466bc70b855a0380ed
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-18 14:41:43 -0700

    make the search page result component

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -7,7 +7,7 @@ function SearchPage() {
   return (
     <div className="searchPage">
       <div className="searchPage__info">
-        <p>62 stays · 26 august to 30 august · 2 guests </p>
+        <p>62 stays · 18 September to 18 October · 2 guests </p>
         <h1>Stays nearby</h1>
         <Button variant="outlined">Cancellation Flexibility</Button>
         <Button variant="outlined">Type of place</Button>
